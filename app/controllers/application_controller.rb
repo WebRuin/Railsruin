e1793eeffa13fa6901d2d0587cd509db9a547ec4
Tim Smith <webruin@icloud.com>
2015-04-26 15:52:05 -0700

    fuck load of clean up and mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Dynamic Page Title
+  def show
+    @post = Post.find_by_id params[:id]
+    @title = "WebRuin | #{@post.title}"
+  end
+  
 end
